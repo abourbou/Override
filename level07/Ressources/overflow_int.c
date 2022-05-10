ddf1c0d91eaad53cd7c8cb67297300fb697ea880
Arthur Bourbousson <abourbou@z4r7p1.42lyon.fr>
2022-05-10 11:57:29 +0200

    continue level07

---
+++ level07/Ressources/overflow_int.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <limits.h>
+
+int	main(void)
+{
+	int i = 0;
+	int nb = -4160441661;
+	int finish = 0;
+
+	printf("we search for i * 4 = %d\n", nb);
+	for (int i = INT_MIN; i != INT_MAX; i++)
+	{
+		if (i * 4 == nb)
+			printf("%d is solution\n", i);
+	}
+	return (0);
+}
